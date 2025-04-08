8d69f76e71ca0a0b999c1e71f8b8c4fc2123d35f
Raphael-stefano <137648063+Raphael-stefano@users.noreply.github.com>
2025-04-08 19:27:18 -0300

    reenviando jogador

---
+++ Jogador.java
@@ -0,0 +1,44 @@
+package br.edu.iff.bancodepalavras.dominio.jogador;
+
+import br.edu.iff.dominio.ObjetoDominioImpl;
+
+public class Jogador extends ObjetoDominioImpl{
+    private String nome;
+    private int pontuacao = 0;
+
+    private Jogador(long id, String nome){
+        super(id);
+        this.nome = nome;
+    }
+
+    private Jogador(long id, String nome, int pontuacao){
+        super(id);
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+    }
+
+    public Jogador criar(long id, String nome){
+        return new Jogador(id, nome);
+    }
+
+    public Jogador reconstruir(long id, String nome, int pontuacao){
+        return new Jogador(id, nome, pontuacao);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+}
