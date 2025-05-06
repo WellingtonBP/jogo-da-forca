a09ee8c9ff1483bd2ed278304b2898870976950a
Reiluc <129343522+Reiluc@users.noreply.github.com>
2025-05-06 12:26:04 -0300

    Main, Banco de Dados e Jogador
    Adicionei um Main para vcs darem uma olhada e ter como base para quando forem fazer, botei em jogador uma sugestão para mesclar melhor com o código do banco de dados e as partes dos pontos e o banco de dados que só esta com dois temas pq queria testar, mas meu Vs code esta todo bugado e tenho que dar uma saída aqui.

---
+++ jogo-da-forca-main/jogo-da-forca-main/boneco/BonecoImagem.java
@@ -0,0 +1,39 @@
+package br.edu.iff.boneco;
+public class BonecoImagem implements Boneco {
+    private static BonecoImagem soleInstance;
+    private long id;
+
+    // Construtor privado
+    private BonecoImagem() {
+        this.id = 2; // Outro id para diferenciar
+    }
+
+    // Retorna a única instância de BonecoImagem
+    public static BonecoImagem getSoleInstance() {
+        if (soleInstance == null) {
+            soleInstance = new BonecoImagem();
+        }
+        return soleInstance;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void exibir(Object contexto, int partes) {
+        // Exemplo: pode-se implementar a exibição a partir de imagens.
+        if(partes == 0) {
+            System.out.println("sem boneco.");
+        } else {
+            System.out.println("o boneco está com " + partes );
+        }
+    }
+}
+// Exemplo de uso
+// BonecoImagem boneco = BonecoImagem.getSoleInstance();
+// boneco.exibir(null, 3); // Exibe: "o boneco está com: 3"
+// System.out.println("ID do boneco: " + boneco.getId()); // Exibe: "ID do boneco: 2"
+// BonecoImagem boneco2 = BonecoImagem.getSoleInstance();
+// System.out.println(boneco == boneco2); // Exibe: "true", pois é a mesma instância
