cef2bfa871c2d4286e358422ab91544337892663
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2025-05-13 02:18:28 -0300

    feat: add palavra bdr repository class

---
+++ src/main/java/br/edu/iff/bancodepalavras/dominio/palavra/embdr/BDRPalavraRepository.java
@@ -0,0 +1,50 @@
+package br.edu.iff.bancodepalavras.dominio.palavra.embdr;
+
+import br.edu.iff.bancodepalavras.dominio.palavra.Palavra;
+import br.edu.iff.bancodepalavras.dominio.palavra.PalavraRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.repository.RepositoryException;
+
+public class BDRPalavraRepository implements PalavraRepository {
+
+  @Override
+  public long getProximoId() {
+    throw new UnsupportedOperationException("Unimplemented method 'getProximoId'");
+  }
+
+  @Override
+  public Palavra getPorId(long id) {
+    throw new UnsupportedOperationException("Unimplemented method 'getPorId'");
+  }
+
+  @Override
+  public Palavra[] getPorTema(Tema tema) {
+    throw new UnsupportedOperationException("Unimplemented method 'getPorTema'");
+  }
+
+  @Override
+  public Palavra[] getTodas() {
+    throw new UnsupportedOperationException("Unimplemented method 'getTodas'");
+  }
+
+  @Override
+  public Palavra getPalavra(String palavra) {
+    throw new UnsupportedOperationException("Unimplemented method 'getPalavra'");
+  }
+
+  @Override
+  public void inserir(Palavra palavra) throws RepositoryException {
+    throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+  }
+
+  @Override
+  public void atualizar(Palavra palavra) throws RepositoryException {
+    throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+  }
+
+  @Override
+  public void remover(Palavra palavra) throws RepositoryException {
+    throw new UnsupportedOperationException("Unimplemented method 'remover'");
+  }
+
+}
