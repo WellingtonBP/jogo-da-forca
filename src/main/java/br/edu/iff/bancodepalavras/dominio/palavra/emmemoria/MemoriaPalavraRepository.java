6b37c107d2063128ff88e630121360713f068024
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2025-05-13 02:03:15 -0300

    feat: add palavra repository

---
+++ src/main/java/br/edu/iff/bancodepalavras/dominio/palavra/emmemoria/MemoriaPalavraRepository.java
@@ -0,0 +1,79 @@
+package br.edu.iff.bancodepalavras.dominio.palavra.emmemoria;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.iff.bancodepalavras.dominio.palavra.Palavra;
+import br.edu.iff.bancodepalavras.dominio.palavra.PalavraRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.repository.RepositoryException;
+
+public class MemoriaPalavraRepository implements PalavraRepository {
+  private static MemoriaPalavraRepository soleInstance;
+  private final Map<Long, Palavra> map;
+  private long sequence = 0;
+
+  private MemoriaPalavraRepository() {
+    this.map = new HashMap<>();
+  }
+
+  public MemoriaPalavraRepository getSoleInstance() {
+    if (MemoriaPalavraRepository.soleInstance == null) {
+      MemoriaPalavraRepository.soleInstance = new MemoriaPalavraRepository();
+    }
+
+    return MemoriaPalavraRepository.soleInstance;
+  }
+
+  @Override
+  public long getProximoId() {
+    return sequence + 1;
+  }
+
+  @Override
+  public Palavra getPorId(long id) {
+    return map.get(id);
+  }
+
+  @Override
+  public Palavra[] getPorTema(Tema tema) {
+    List<Palavra> palavras = new ArrayList<>();
+
+    for (var palavra : map.values()) {
+      if (palavra.getTema().equals(tema)) {
+        palavras.add(palavra);
+      }
+    }
+
+    return palavras.toArray(new Palavra[palavras.size()]);
+  }
+
+  @Override
+  public Palavra[] getTodas() {
+    return map.values().toArray(new Palavra[map.values().size()]);
+  }
+
+  @Override
+  public Palavra getPalavra(String palavra) {
+    return map.values().stream().filter(p -> p.comparar(palavra)).findFirst().orElse(null);
+  }
+
+  @Override
+  public void inserir(Palavra palavra) throws RepositoryException {
+    map.put(this.getProximoId(), palavra);
+    this.sequence++;
+  }
+
+  @Override
+  public void atualizar(Palavra palavra) throws RepositoryException {
+    map.put(palavra.getId(), palavra);
+  }
+
+  @Override
+  public void remover(Palavra palavra) throws RepositoryException {
+    map.remove(palavra.getId());
+  }
+
+}
---
+++ src/main/java/br/edu/iff/repository/RepositoryException.java
@@ -0,0 +1,11 @@
+package br.edu.iff.repository;
+
+public class RepositoryException extends Exception {
+  public RepositoryException() {
+    super();
+  }
+
+  public RepositoryException(String message) {
+    super(message);
+  }
+}
