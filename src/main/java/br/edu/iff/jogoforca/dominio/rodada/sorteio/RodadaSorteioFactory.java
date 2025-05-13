24f2ffeed9fef89209a0baca9c42712558c1d9cb
Wellington Pacheco <wellingtonpacheco14@gmail.com>
2025-05-13 04:03:28 -0300

    fix: rodada sorteio factory impl

--- src/main/java/br/edu/iff/jogoforca/dominio/rodada/sorteio/RodadaSorteioFactory.java
+++ src/main/java/br/edu/iff/jogoforca/dominio/rodada/sorteio/RodadaSorteioFactory.java
@@ -8,6 +8,7 @@ import br.edu.iff.jogoforca.dominio.jogador.Jogador;
 import br.edu.iff.jogoforca.dominio.rodada.Rodada;
 import br.edu.iff.jogoforca.dominio.rodada.RodadaFactoryImpl;
 import br.edu.iff.jogoforca.dominio.rodada.RodadaRepository;
+import br.edu.iff.repository.RepositoryException;
 
 public class RodadaSorteioFactory extends RodadaFactoryImpl {
   private static RodadaSorteioFactory soleInstance;
@@ -27,22 +28,21 @@ public class RodadaSorteioFactory extends RodadaFactoryImpl {
     return RodadaSorteioFactory.soleInstance;
   }
 
-  public Rodada getRodada(Jogador jogador) {
-    return this.getRodadaRepository().getPorJogador(jogador);
-    // var existente = this.getRodadaRepository().getPorJogador(jogador);
-    // if (existente != null) {
-    // return existente;
-    // }
+  public Rodada getRodada(Jogador jogador) throws RepositoryException {
+    var existente = this.getRodadaRepository().getPorJogador(jogador);
+    if (existente != null) {
+      return existente;
+    }
 
-    // var temas = this.getTemaRepository().getTodos();
-    // if (temas.length != 0) {
-    // int randomIndexTema = ThreadLocalRandom.current().nextInt(temas.length);
-    // var sortedTema = temas[randomIndexTema];
-    // var palavras = this.getPalavraRepository().getPorTema(sortedTema);
-    // int randomIndexPalavra =
-    // ThreadLocalRandom.current().nextInt(palavras.length);
-    // var sortedPalavra = palavras[randomIndexPalavra];
-    // }
-    // return null;
+    var temas = this.getTemaRepository().getTodos();
+    if (temas.length != 0) {
+      var randomIndexTema = ThreadLocalRandom.current().nextInt(temas.length);
+      var sortedTema = temas[randomIndexTema];
+      var palavras = this.getPalavraRepository().getPorTema(sortedTema);
+      var rodada = Rodada.criar(getProximoId(), palavras, jogador);
+      this.getRodadaRepository().inserir(rodada);
+      return rodada;
+    }
+    return null;
   }
 }
